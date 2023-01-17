1e15090dd676e2c69e29fca659bdb24f5da615e5
Josue Ale <jam9814@hotmail.com>
2023-01-17 11:10:59 -0500

    fix: deleted js files

--- src/config/keys.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoURI = void 0;
-const secrets_1 = require("@Helpers/secrets");
-exports.mongoURI = secrets_1.MONGO_URI;
